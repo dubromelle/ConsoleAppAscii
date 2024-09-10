031a6fbab997f6f73ba0bfcf5be571c544cf0676
Jérôme Dubromelle <jerome.dubromelle@ac-amiens.fr>
2024-09-10 10:57:38 +0200

    Ajoutez des fichiers projet.

---
+++ ConsoleAppAscii/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAscii
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("              ....");
+            Console.WriteLine("                                .''.'''");
+            Console.WriteLine(".                             .'   :");
+            Console.WriteLine("\\                          .:    :");
+            Console.WriteLine(" \\                        _:    :       ..----.._");
+            Console.WriteLine("  \\                    .:::.....:::.. .'         ''.");
+            Console.WriteLine("   \\                 .'  #-. .-######'     #        '.");
+            Console.WriteLine("    \\                 '.##' / ' ################       :");
+            Console.WriteLine("     \\                  #####################         :");
+            Console.WriteLine("      \\               ..##.-.#### .''''###'.._        :");
+            Console.WriteLine("       \\             :--:########:            '.    .' :");
+            Console.WriteLine("        \\..__...--.. :--:#######.'   '.         '.     :");
+            Console.WriteLine("        :     :  : : '':'-:'':'::        .         '.  .'");
+            Console.WriteLine("         '---'''..: :    ':    '..'''.      '.        :'");
+            Console.WriteLine("           \\  :: : :     '      ''''''.     '.      .:");
+            Console.WriteLine("            \\ ::  : :     '            '.      '      :");
+            Console.WriteLine("             \\::   : :           ....' ..:       '     '.");
+            Console.WriteLine("              \\::  : :    .....####\\ .~~.:.             :");
+            Console.WriteLine("              \\':.:.:.:'#########.===. ~ |.'-.   . '''.. :");
+            Console.WriteLine("                \\    .'  ########## \\ \\ _.' '. ' -.       '''.");
+            Console.WriteLine("                :\\  :     ########   \\ \\      '.  '-.        :");
+            Console.WriteLine("               :  \\'    '   #### :    \\ \\      :.    '-.      :");
+            Console.WriteLine("              :  .'\\   :'  :     :     \\ \\       :      '-.    :");
+            Console.WriteLine("             : .'  .\\  '  :      :     :\\ \\       :        '.   :");
+            Console.WriteLine("             ::   :  \\'  :.      :     : \\ \\      :          '. :");
+            Console.WriteLine("             ::. :    \\  : :      :    ;  \\ \\     :           '.:");
+            Console.WriteLine("              : ':    '\\ :  :     :     :  \\:\\     :        ..'");
+            Console.WriteLine("                 :    ' \\ :        :     ;  \\|      :   .'''");
+            Console.WriteLine("                 '.   '  \\:                         :.''");
+            Console.WriteLine("                  .:..... \\:       :            ..''");
+            Console.WriteLine("                 '._____|'.\\......'''''''.:..'''");
+            Console.WriteLine("                            \\");
+            Console.WriteLine();
+            Console.WriteLine("YOU SHALL NOT PASS ! ");
+
+           Console.ReadKey();
+
+
+
+
+        }
+    }
+}
